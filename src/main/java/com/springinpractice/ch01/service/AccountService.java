07609de976073ea580b2bd07d11f14c818b0aa98
Joshua White <josh@jwhitesolutions.com>
2012-02-26 17:33:08 -0500

    Initial commit for branch 03

--- src/main/java/com/springinpractice/ch01/service/AccountService.java
+++ src/main/java/com/springinpractice/ch01/service/AccountService.java
@@ -1,30 +1,14 @@
-//Source project: sip01, branch: 02 (Maven Project)
+//Source project: sip01, branch: 03 (Maven Project)
 package com.springinpractice.ch01.service;
-import java.util.Properties;
-import java.io.InputStream;
-import org.apache.commons.dbcp.BasicDataSource;
-import com.springinpractice.ch01.dao.jdbc.JdbcAccountDao;       
+import com.springinpractice.ch01.dao.AccountDao;       
 
 public class AccountService {
-    private JdbcAccountDao accountDao;
+    private AccountDao accountDao;
 
-    public AccountService() {    	
-    	try {
-            Properties props = new Properties();
-            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("dataSource.properties");
-            props.load(inputStream);
-    
-            BasicDataSource dataSource = new BasicDataSource();
-            dataSource.setDriverClassName(props.getProperty("driverClassName"));
-            dataSource.setUrl(props.getProperty("url"));
-            dataSource.setUsername(props.getProperty("username"));
-            dataSource.setPassword(props.getProperty("password"));
+    public AccountService() {}
 
-        	accountDao = new JdbcAccountDao();
-            accountDao.setDataSource(dataSource);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public void setAccountDao(AccountDao accountDao) {
+		this.accountDao = accountDao;
+	}
+        
 }
-
