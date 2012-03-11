8ea2197a4153825452d6038355f86a753c11de1d
Joshua White <josh@jwhitesolutions.com>
2012-03-11 10:42:29 -0400

    Formatting changes

--- src/main/java/com/springinpractice/ch01/dao/jdbc/JdbcAccountDao.java
+++ src/main/java/com/springinpractice/ch01/dao/jdbc/JdbcAccountDao.java
@@ -11,7 +11,8 @@ public class JdbcAccountDao implements AccountDao {
 	public JdbcAccountDao() {
 		dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");		
-		dataSource.setUrl("jdbc:mysql://localhost:3306/springbook?autoReconnect=true");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/springbook" + 
+				"?autoReconnect=true");
 		dataSource.setUsername("root");
 		dataSource.setPassword("");
 	}	
