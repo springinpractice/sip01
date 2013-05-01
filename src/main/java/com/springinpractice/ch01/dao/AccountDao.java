3068b611ad366db2c1a763f48ef5fb8766630252
Willie Wheeler <willie.wheeler@gmail.com>
2013-05-01 00:53:57 -0700

    Cleanup

--- src/main/java/com/springinpractice/ch01/dao/AccountDao.java
+++ src/main/java/com/springinpractice/ch01/dao/AccountDao.java
@@ -1,6 +1,11 @@
-// Source project: sip01, branch: 01 (Maven Project)
+/* 
+ * Copyright (c) 2013 Manning Publications Co.
+ * 
+ * Book: http://manning.com/wheeler/
+ * Blog: http://springinpractice.com/
+ * Code: https://github.com/springinpractice
+ */
 package com.springinpractice.ch01.dao;
 
 public interface AccountDao {
-
 }
